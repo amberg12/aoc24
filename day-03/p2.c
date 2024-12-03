f89f23b8abfd0185462a68c357ae4d0a9cda3c29
Amber Goulding <gouldinga1@cardiff.ac.uk>
2024-12-03 09:58:55 +0000

    day 03 part 02

--- day-03/p2.c
+++ day-03/p2.c
@@ -1,7 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define BIG_MEM_BLOCK_LEN (1024 * 1024 * 1024)
+
+static long long int
+check_mult_from(char *cursor);
+
+static long long int
+check_mult_from(char *cursor)
+{
+	long long int lhs = 0;
+	long long int rhs = 0;
+
+	if (memcmp(cursor, "mul(", 4) != 0) {
+		return 0;
+	}
+	cursor += 4;
+
+	bool fdig = false;
+	while (isdigit(*cursor)) {
+		lhs *= 10;
+		lhs += *cursor & 15;
+		++cursor;
+		fdig = true;
+	}
+
+	if (*cursor != ',' || !fdig) {
+		return 0;
+	}
+
+	cursor++;
+	fdig = false;
+	while (isdigit(*cursor)) {
+		rhs *= 10;
+		rhs += *cursor & 15;
+		++cursor;
+		fdig = true;
+	}
+
+	if (*cursor != ')' || !fdig) {
+		return 0;
+	}
+	return lhs * rhs;
+}
 
 int
 main(int argc, char **argv)
 {
-	printf("Hello, world!");
+	long long int sol = 0;
+	char *scanbuf = malloc(BIG_MEM_BLOCK_LEN);
+
+	bool do_mode = true;
+
+	while (fgets(scanbuf, BIG_MEM_BLOCK_LEN, stdin)) {
+		char *cursor = scanbuf;
+
+		while (*cursor) {
+			if (memcmp(cursor, "do()", 4) == 0) do_mode = true;
+			if (memcmp(cursor, "don't()", 7) == 0) do_mode = false;
+
+			if (do_mode) sol += check_mult_from(cursor);
+			cursor++;
+		}
+
+		memset(scanbuf, 0, BIG_MEM_BLOCK_LEN);
+	}
+
+	printf("%lld\n", sol);
 }
