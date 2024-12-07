e3a77141f9542c653cd4c87bd2e1022e7f5c1304
Amber Goulding <gouldinga1@cardiff.ac.uk>
2024-12-07 15:17:53 +0000

    day 07 part 02

--- day-07/p2.c
+++ day-07/p2.c
@@ -1,7 +1,76 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+static inline long long int
+parsenum(const char *);
+
+static inline bool
+can_valid(const char *c, long long int target);
+
+static inline long long int
+parsenum(const char *c) {
+	long long int out = 0;
+	while ('0' <= *c && *c <= '9') {
+		out *= 10;
+		out += *c & 15;
+		c++;
+	}
+	return out;
+}
+
+static inline bool
+numslist_can_be_target(
+	long long int nums[],
+	size_t nelems,
+	int i,
+	long long int root,
+	long long int target
+) {
+	if (i == nelems) return root == target;
+	char catbuf[100] = {0};
+
+	long long int mult = root * nums[i];
+	long long int add = root + nums[i];
+	sprintf(catbuf, "%lld%lld", root, nums[i]);
+	long long int cat = parsenum(catbuf);
+
+	bool mult_ok = numslist_can_be_target(nums, nelems, i+1, mult, target);
+	bool add_ok = numslist_can_be_target(nums, nelems, i+1, add, target);
+	bool cat_ok = numslist_can_be_target(nums, nelems, i+1, cat, target);
+
+	return mult_ok || add_ok || cat_ok;
+}
+
+static inline bool
+can_valid(const char *c, long long int target)
+{
+	long long int nums[128] = {0};
+	size_t nelems = 0;
+	while (*c) {
+		nums[nelems++] = parsenum(c);
+		while (*c != ' ' && *c != '\n') {
+			c++;
+		}
+		c++;
+	}
+
+	return numslist_can_be_target(nums, nelems, 1, nums[0], target);
+}
 
 int
 main(int argc, char **argv)
 {
-	printf("Hello, world!");
+	long long int sol = 0;
+
+	char scanbuf[256] = {0};
+	while (fgets(scanbuf, sizeof(scanbuf), stdin)) {
+		char *cursor = scanbuf;
+		long long int leftnum = parsenum(cursor);
+		while (*cursor++ != ' ');
+		if (can_valid(cursor, leftnum)) sol += leftnum;
+		memset(scanbuf, 0, sizeof(scanbuf));
+	}
+
+	printf("%lld\n", sol);
 }
